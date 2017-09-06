84975a316ca002bdfec0383362a87d715319ec22
yunanhelmi <yunanhelmimahendra@gmail.com>
2017-09-06 20:12:50 +0700

    Menambahkan sub menu Pemesanan Layanan

---
+++ views/site/layanan.php
@@ -0,0 +1,157 @@
+<?php
+
+use yii\helpers\Html;
+$this->title = 'Pemesanan Layanan';
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+
+<div class="container">
+    <h1 align="center">Pemesanan Layanan</h1>
+    <br>
+    <h3>Detail Harga Pemesanan Layanan</h3>
+    <p>Berikut adalah rincian equipment yang kami sediakan/rental untuk mendukung pelaksanaan kegiatan suatu acara, diantaranya:</p>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>No. </th>
+                <th>Spesifikasi</th>
+                <th>Harga</th>
+                <th>Satuan</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1. </td>
+                <td>Tenda / terop ukuran 4x6 m<sup>2</sup></td>
+                <td>Rp. 150.000,00</td>
+                <td>Unit</td>
+            </tr>
+            <tr>
+                <td>2. </td>
+                <td>Barikade tinggi 1,5 m, panjang per unit bervariasi</td>
+                <td>Rp. 17.000,00</td>
+                <td>Meter</td>
+            </tr>
+            <tr>
+                <td>3. </td>
+                <td>Lampu TL untuk masing-masing tenda</td>
+                <td>Rp. 40.000,00</td>
+                <td>Unit</td>
+            </tr>
+            <tr>
+                <td>4. </td>
+                <td>Genset khusus untuk seluruh tenda</td>
+                <td>Rp. 1.000.000,00</td>
+                <td>Unit</td>
+            </tr>
+            <tr>
+                <td rowspan="6">5. </td>
+                <td>Sound system paket A:</td>
+                <td rowspan="6">Rp. 3.000.000,00</td>
+                <td rowspan="6">Paket</td>
+            </tr>
+            <tr>
+                <td>- Sound system 15.000 Watt dengan konfigurasi ground stack (speaker tumpuk)</td>
+            </tr>
+            <tr>
+                <td>- Stage 4x6</td>
+            </tr>
+            <tr>
+                <td>- Lighting LED Par</td>
+            </tr>
+            <tr>
+                <td>- Genset</td>
+            </tr>
+            <tr>
+                <td>- Instrument musik full original</td>
+            </tr>
+            <tr>
+                <td rowspan="6">6. </td>
+                <td>Sound system paket B:</td>
+                <td rowspan="6">Rp. 6.250.000,00</td>
+                <td rowspan="6">Paket</td>
+            </tr>
+            <tr>
+                <td>- Sound system >50.000 Watt dengan konfigurasi full rigging (speaker gantung)</td>
+            </tr>
+            <tr>
+                <td>- Stage 8x6 standart konser</td>
+            </tr>
+            <tr>
+                <td>- Lighting LED Par</td>
+            </tr>
+            <tr>
+                <td>- Genset</td>
+            </tr>
+            <tr>
+                <td>- Instrument musik full original</td>
+            </tr>
+            <tr>
+                <td>7. </td>
+                <td>Pembuatan banner acara full colour standart media promo</td>
+                <td>Rp. 14.500,00</td>
+                <td>Meter</td>
+            </tr>
+        </tbody>
+    </table>
+    <p>*) Harga genset dan atau paket yang menggunakan genset, telah include dengan bensin/solar.</p>
+    <p>**) Jika terdapat permintaan atau kebutuhan acara diluar list kami diatas, maka dapat dikoordinasikan selanjutnya dengan kami.</p>
+    <p>***) Kami masih memiliki spesifikasi equipment khusus peripheral sound system dan stage lebih besar daripada paket B, maka dapat dikoordinasikan selanjutnya dengan kami.</p>
+    <p>****) Khusus untuk multimedia dan atau kebutuhan diluar list, kami menggunakan jasa pihak ke 3, dan untuk budgeting dan konsultasi dapat dikoordinasikan selanjutnya dengan kami.</p>
+    <br>
+    
+    <h3>Dokumentasi Panggung dan Equipment</h3>
+    <div class="row">
+        <h4 align="center">Panggung 4x6</h4>
+        <div class="col-sm-12 col-md-4">
+            <div class="thumbnail">
+                <img style="height:200px" src="img/panggung_4x6_1.png" alt="panggung_4x6_1">
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <div class="thumbnail">
+                <img style="height:200px" src="img/panggung_4x6_2.png" alt="panggung_4x6_2">
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <div class="thumbnail">
+                <img style="height:200px" src="img/panggung_4x6_3.png" alt="panggung_4x6_3">
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <h4 align="center">Panggung 8x6 dan 12x10 Full Rigging</h4>
+        <div class="col-sm-12 col-md-4">
+            <div class="thumbnail">
+                <img style="height:200px" src="img/panggung_8x6_1.png" alt="panggung_8x6_1">
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <div class="thumbnail">
+                <img style="height:200px" src="img/panggung_8x6_2.png" alt="panggung_8x6_2">
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <div class="thumbnail">
+                <img style="height:200px" src="img/panggung_8x6_3.png" alt="panggung_8x6_3">
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-6">
+            <div class="thumbnail">
+                <img style="height:200px; width:800px" src="img/panggung_8x6_4.png" alt="panggung_8x6_4">
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <div class="thumbnail">
+                <img style="height:200px; width:800px" src="img/panggung_8x6_5.png" alt="panggung_8x6_5">
+            </div>
+        </div>
+    </div>
+    <br>
+    <h3>Contact Person</h3>
+    <p>Jika terdapat beberapa pertanyaan, dapat menghubungi humas kami, saudara AMIRUL MAULIDIHAQ di nomor HP 083847993637. Atas perhatian yang diberikan, kami ucapkan terima kasih.</p>
+</div>
+
